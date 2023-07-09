27ed6d8a34433a7180a9ba45696460d48b21b995
Damian2057 <ddamian2057@gmail.com>
2023-07-09 19:39:14 +0200

    .env configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from "@nestjs/common";
+import * as process from "process";
 
-async function bootstrap() {
+async function main() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(process.env.APP_PORT);
+  console.log(`Application is running on: ${process.env.APP_PORT}`);
 }
-bootstrap();
+main();
