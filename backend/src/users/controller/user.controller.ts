99c005ea7d87c7a92ce75dfc4e43c78cb12ae669
Damiaan <ddamian2057@gmail.com>
2023-07-19 22:15:23 +0200

    adding plantuml in architecture module

--- backend/src/users/controller/user.controller.ts
+++ backend/src/users/controller/user.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get } from "@nestjs/common";
 import { UserService } from "../service/user.service";
-import { ForbiddenException } from "../../exceptions/type/forbidden.exception";
 
 @Controller()
 export class UserController {
--- backend/src/users/model/domain/user.entity.ts
+++ backend/src/users/model/domain/user.entity.ts
@@ -1,24 +1,27 @@
 import { Column, Entity } from "typeorm";
 import { AbstractEntity } from "../../../database/abstract.entity";
 import { UserRoleEntity } from "./user.role.entity";
+import { IsEmail, Length } from "class-validator";
 
 @Entity()
 export class User extends AbstractEntity<User> {
 
   @Column({ length: 50 })
+  @Length(4, 50)
   username: string
 
   @Column({ length: 50 })
+  @Length(8, 50)
   password: string
 
   @Column({ length: 50 })
+  @IsEmail()
   email: string
 
   @Column({
     type: "enum",
     enum: UserRoleEntity,
     default: UserRoleEntity.USER
-  }
-  )
+  })
   role: UserRoleEntity
 }
