bd84fcbe40e3b9690dd995300d8dd5450249f427
Damiaan <ddamian2057@gmail.com>
2023-07-24 22:19:55 +0200

    primary token with role-based authorization

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,8 +1,14 @@
-import { Module } from "@nestjs/common";
+import { forwardRef, Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
+import { AuthService } from './service/auth.service';
+import { UserModule } from "../users/user.module";
+import { JwtGuard } from "./guard/jwt.guard";
+import { RolesGuard } from "./guard/roles.guard";
+import { JwtStrategy } from "./guard/jwt.strategy.guard";
 
 @Module({
   imports: [
+    forwardRef(() => UserModule),
     JwtModule.registerAsync({
       useFactory: () => ({
         secret: process.env.JWT_SECRET,
@@ -10,7 +16,7 @@ import { JwtModule } from "@nestjs/jwt";
       })
     })
   ],
-  providers: [],
-  exports: []
+  providers: [AuthService, JwtGuard, RolesGuard, JwtStrategy],
+  exports: [AuthService]
 })
 export class AuthModule {}
--- backend/src/users/controller/user.controller.ts
+++ backend/src/users/controller/user.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, UseGuards } from "@nestjs/common";
 import { UserService } from "../service/user.service";
+import { hasRoles } from "../../auth/decorator/role.decorator";
+import { RolesGuard } from "../../auth/guard/roles.guard";
+import { JwtGuard } from "../../auth/guard/jwt.guard";
 
 @Controller()
 export class UserController {
@@ -7,6 +10,8 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
+  @hasRoles("admin")
+  @UseGuards(RolesGuard, JwtGuard)
   getHello(): string {
     return "Hello World!";
   }
