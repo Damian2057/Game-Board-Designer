55cd8e8227dba890f229ff5a3d8b5b713072b192
Damiaan <ddamian2057@gmail.com>
2023-08-01 21:03:11 +0200

    adding login and token generation

--- backend/src/auth/model/dto/auth.token.dto.ts
+++ backend/src/auth/model/dto/auth.token.dto.ts
@@ -1,3 +1,9 @@
 export class AuthTokenDto {
+  expiresIn: string;
+  token: string;
 
+  constructor(expiresIn: string, token: string) {
+    this.expiresIn = expiresIn;
+    this.token = token;
+  }
 }
--- backend/src/auth/service/auth.service.ts
+++ backend/src/auth/service/auth.service.ts
@@ -3,6 +3,10 @@ import { JwtService } from "@nestjs/jwt";
 import { User } from "../../users/model/domain/user.entity";
 import {UserService} from "../../users/service/user.service";
 import { AuthLoginCommand } from "../model/command/auth.login.command";
+import { AuthTokenDto } from "../model/dto/auth.token.dto";
+import * as process from "process";
+import { IncorrectLoginCredentialsException } from "../../exceptions/type/incorrect.login.credentials.exception";
+import { comparePasswords } from "../util/util.functions";
 
 @Injectable()
 export class AuthService {
@@ -15,7 +19,11 @@ export class AuthService {
     return this.jwtService.signAsync({user});
   }
 
-  login(command: AuthLoginCommand) {
-    return null;
+  async login(command: AuthLoginCommand) {
+    const user = await this.userService.findOneByUsername(command.username);
+    if (user && await comparePasswords(command.password, user.password)) {
+      return new AuthTokenDto(process.env.JWT_EXPIRATION_TIME, await this.createToken(user));
+    }
+    throw new IncorrectLoginCredentialsException();
   }
 }
