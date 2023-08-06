958c7e2b8407d7c4aff49d383634151033a16be4
Damiaan <ddamian2057@gmail.com>
2023-08-06 20:31:09 +0200

    adding multer, starting the implementation of game management modules

--- backend/src/board-game/board-game.module.ts
+++ backend/src/board-game/board-game.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { BoardGameController } from './controller/board-game.controller';
+import { BoardGameService } from './service/board-game.service';
 
-@Module({})
+@Module({
+  controllers: [BoardGameController],
+  providers: [BoardGameService]
+})
 export class BoardGameModule {}
