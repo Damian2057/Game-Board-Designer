78cb0c3ef7287cf0e58774ae6043f7a1ef305ff6
Damiaan <ddamian2057@gmail.com>
2023-08-07 18:37:46 +0200

    service injection into the module controller

--- backend/src/board-game/service/board-game.service.ts
+++ backend/src/board-game/service/board-game.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { BoardGame } from "../model/domain/board-game.entity";
+import { Tag } from "../model/domain/tag.entity";
+import { GameElement } from "../model/domain/game.element.entity";
 
 @Injectable()
-export class BoardGameService {}
+export class BoardGameService {
+
+  constructor(
+    @InjectRepository(BoardGame)
+    private readonly boardGameRepository: Repository<BoardGame>,
+    @InjectRepository(Tag)
+    private readonly tagRepository: Repository<Tag>,
+    @InjectRepository(GameElement)
+    private readonly gameElementRepository: Repository<GameElement>
+  ) {}
+
+
+}
