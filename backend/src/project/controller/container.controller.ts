6ecb78a280a7611821307ae4703e8340f472c8d1
Damiaan <ddamian2057@gmail.com>
2023-08-22 21:57:25 +0200

    + container and element controllers template

--- backend/src/project/controller/container.controller.ts
+++ backend/src/project/controller/container.controller.ts
@@ -1,5 +1,8 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 import { ContainerService } from "../service/container.service";
+import { ContainerDto } from "../model/dto/container.dto";
+import { UpdateContainerCommand } from "../model/command/container/update.container.command";
+import { CreateContainerCommand } from "../model/command/container/create.container.command";
 
 @Controller('container')
 export class ContainerController {
@@ -8,5 +11,34 @@ export class ContainerController {
     private readonly containerService: ContainerService,
   ) {}
 
+  @Put('update-container/:containerId')
+  async updateContainer(@Body() command: UpdateContainerCommand, @Param('containerId') containerId: number): Promise<ContainerDto> {
+    return this.containerService.updateContainer(command, containerId);
+  }
 
+  @Post('add-container/:projectId')
+  async addContainer(@Body() command: CreateContainerCommand, @Param('projectId') projectId: number): Promise<ContainerDto> {
+    return this.containerService.addContainer(command, projectId);
+  }
+
+  @Get(':containerId')
+  async getContainerById(@Param('containerId') containerId: number): Promise<ContainerDto> {
+    return this.containerService.getContainerById(containerId);
+  }
+
+  @Get('containers/:projectId')
+  async getAllContainersByProjectId(@Param('projectId') projectId: number): Promise<ContainerDto[]> {
+    return this.containerService.getAllContainersByProjectId(projectId);
+  }
+
+
+  @Delete('delete-container/:containerId')
+  async deleteContainerById(@Param('containerId') containerId: number): Promise<void> {
+    return this.containerService.deleteContainerById(containerId);
+  }
+
+  @Delete('delete-containers/:projectId')
+  async deleteAllContainersByProjectId(@Param('projectId') projectId: number): Promise<void> {
+    return this.containerService.deleteAllContainersByProjectId(projectId);
+  }
 }
--- backend/src/project/controller/element.controller.ts
+++ backend/src/project/controller/element.controller.ts
@@ -1,5 +1,8 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 import { ElementService } from "../service/element.service";
+import { ElementDto } from "../model/dto/element.dto";
+import { UpdateElementCommand } from "../model/command/element/update.element.command";
+import { CreateElementCommand } from "../model/command/element/create.element.command";
 
 @Controller('element')
 export class ElementController {
@@ -7,4 +10,39 @@ export class ElementController {
   constructor(
     private readonly elementService: ElementService,
   ) {}
+
+  @Put('update-element/:elementId')
+  async updateElement(@Body() command: UpdateElementCommand, @Param('elementId') elementId: number): Promise<ElementDto> {
+    return this.elementService.updateElement(command, elementId);
+  }
+
+  @Post('add-container-element/:containerId')
+  async addContainerElement(@Body() command: CreateElementCommand, @Param('containerId') containerId: number): Promise<ElementDto> {
+    return this.elementService.addContainerElement(command, containerId);
+  }
+
+  @Post('add-project-element/:projectId')
+  async addProjectElement(@Body() command: CreateElementCommand, @Param('projectId') projectId: number): Promise<ElementDto> {
+    return this.elementService.addProjectElement(command, projectId);
+  }
+
+  @Get(':elementId')
+  async getElementById(@Param('elementId') elementId: number): Promise<ElementDto> {
+    return this.elementService.getElementById(elementId);
+  }
+
+  @Get('containers-elements/:containerId')
+  async getAllContainerElementsByContainerId(@Param('containerId') containerId: number): Promise<ElementDto[]> {
+    return this.elementService.getAllContainerElementsByContainerId(containerId);
+  }
+
+  @Get('project-elements/:projectId')
+  async getAllProjectElementsByProjectId(@Param('projectId') projectId: number): Promise<ElementDto[]> {
+    return this.elementService.getAllProjectElementsByProjectId(projectId);
+  }
+
+  @Delete('delete-element/:elementId')
+  async deleteElementById(@Param('elementId') elementId: number): Promise<void> {
+    return this.elementService.deleteElementById(elementId);
+  }
 }
