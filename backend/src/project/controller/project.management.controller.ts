ec46a5cbfcd911ed1c39fe326ec899136e55ff72
Damiaan <ddamian2057@gmail.com>
2023-08-22 21:43:44 +0200

    + ProjectManagementController

--- backend/src/project/controller/project.management.controller.ts
+++ backend/src/project/controller/project.management.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Param, Post, Put } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Put } from "@nestjs/common";
 import { GetCurrentUser } from "../../auth/decorator/current.user.decorator";
 import { ProjectManagementService } from "../service/project.management.service";
+import { UpdateProjectCommand } from "../model/command/project-management/update.project.command";
+import { ProjectDto } from "../model/dto/project.dto";
 
 @Controller('project')
 export class ProjectManagementController {
@@ -10,43 +12,43 @@ export class ProjectManagementController {
   ) {}
 
   @Post('start-new-project/:projectId')
-  async createNewProjectBasedOnExistingProject(@GetCurrentUser() user, @Param('projectId') projectId: number) {
-    return
+  async createNewProjectBasedOnExistingProject(@GetCurrentUser() user, @Param('projectId') projectId: number): Promise<ProjectDto> {
+    return this.projectManagementService.createNewProjectBasedOnExistingProject(user, projectId);
   }
 
 
   @Get('my-projects')
-  async getMyProjects(@GetCurrentUser() user) {
-
+  async getMyProjects(@GetCurrentUser() user): Promise<ProjectDto[]> {
+    return this.projectManagementService.getMyProjects(user);
   }
 
   @Get('my-completed-projects')
-  async getMyCompletedProjects(@GetCurrentUser() user) {
-
+  async getMyCompletedProjects(@GetCurrentUser() user): Promise<ProjectDto[]> {
+    return this.projectManagementService.getMyCompletedProjects(user);
   }
 
   @Get('my-ongoing-projects')
-  async getMyOngoingProjects(@GetCurrentUser() user) {
-
+  async getMyOngoingProjects(@GetCurrentUser() user): Promise<ProjectDto[]> {
+    return this.projectManagementService.getMyOngoingProjects(user);
   }
 
   @Get('all-projects/:userId')
-  async getAllProjectsForUser(@Param('userId') userId: number) {
-
+  async getAllProjectsForUser(@Param('userId') userId: number): Promise<ProjectDto[]> {
+    return this.projectManagementService.getAllProjectsForUser(userId);
   }
 
   @Get('all-completed-projects/:userId')
-  async getAllCompletedProjects(@Param('userId') userId: number) {
-
+  async getAllCompletedProjects(@Param('userId') userId: number): Promise<ProjectDto[]> {
+    return this.projectManagementService.getAllCompletedProjects(userId);
   }
 
   @Get('all-ongoing-projects/:userId')
-  async getAllOngoingProjects(@Param('userId') userId: number) {
-
+  async getAllOngoingProjects(@Param('userId') userId: number): Promise<ProjectDto[]> {
+    return this.projectManagementService.getAllOngoingProjects(userId);
   }
 
   @Put('update-project/:projectId')
-  async updateProject(@Param('projectId') projectId: number) {
-
+  async updateProject(@Body() command: UpdateProjectCommand, @Param('projectId') projectId: number): Promise<ProjectDto> {
+    return this.projectManagementService.updateProject(command, projectId);
   }
 }
