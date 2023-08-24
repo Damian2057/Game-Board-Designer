0d8f61a4e7fee2c1eb24167624397ff3fb9c05ac
Damiaan <ddamian2057@gmail.com>
2023-08-24 22:04:45 +0200

    Update create.project.command.ts

--- backend/src/project/model/command/project-creator/create.project.command.ts
+++ backend/src/project/model/command/project-creator/create.project.command.ts
@@ -1,3 +1,18 @@
-export class CreateProjectCommand {
+import { BoxDto } from "../../dto/box.dto";
+import { ContainerDto } from "../../dto/container.dto";
+import { ElementDto } from "../../dto/element.dto";
+import { GameDto } from "../../../../game/model/dto/game.dto";
 
+export class CreateProjectCommand {
+  name: string;
+  description: string;
+  notes: string[];
+  box: BoxDto;
+  containers: ContainerDto[];
+  elements: ElementDto[];
+  games: GameDto[];
+  currentGame: GameDto;
+  isTemplate: boolean;
+  imageIds: number[];
+  isCompleted: boolean;
 }
