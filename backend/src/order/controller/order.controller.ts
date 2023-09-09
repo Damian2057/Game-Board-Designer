c22dc4ea2016533d14fb7af0af8aefbdf2bf1b3d
Damiaan <ddamian2057@gmail.com>
2023-09-09 15:34:42 +0200

    creating an API for placing orders

--- backend/src/order/controller/order.controller.ts
+++ backend/src/order/controller/order.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put } from "@nestjs/common";
+import { OrderService } from "../service/order.service";
 
 @Controller('order')
-export class OrderController {}
+export class OrderController {
+
+  constructor(
+    private readonly orderService: OrderService,
+  ) {
+  }
+
+  @Post('submit')
+  async submitOrder() {
+
+  }
+
+  @Get('my-orders')
+  async getMyOrders() {
+
+  }
+
+  @Get('user-orders/:id')
+  async getUserOrders() {
+
+  }
+
+  @Get('all-orders')
+  async getAllOrders() {
+
+  }
+
+  @Get(':id')
+  async getOrderById() {
+
+  }
+
+  @Put(':id')
+  async updateOrder() {
+
+  }
+}
--- backend/src/order/controller/order.management.controller.ts
+++ backend/src/order/controller/order.management.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from "@nestjs/common";
+import { OrderService } from "../service/order.service";
 
 @Controller('order/management')
-export class OrderManagementController {}
+export class OrderManagementController {
+
+  constructor(
+    private readonly orderService: OrderService,
+  ) {
+  }
+
+  @Post('claim')
+  async claimOrder() {
+
+  }
+
+}
