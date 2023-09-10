0e12b9890917410d3913e8812a768b11afba9be4
Damiaan <ddamian2057@gmail.com>
2023-09-10 20:48:41 +0200

    CreateOrderCommand validation

--- backend/src/order/model/command/create.order.command.ts
+++ backend/src/order/model/command/create.order.command.ts
@@ -1,11 +1,22 @@
 import { GameDto } from "../../../game/model/dto/game.dto";
+import { Length, Min } from "class-validator";
 
 export class CreateOrderCommand {
 
+  @Length(9, 15)
   phone: string;
+
+  @Length(3, 100)
   email: string;
+
+  @Length(0, 2000)
   description: string;
+
+  @Min(0)
   price: number;
+
+  @Length(3, 100)
   address: string;
+
   game: GameDto;
 }
