cbcaf0031ee1a4c8a65541a97183033244e72fbf
Damiaan <ddamian2057@gmail.com>
2023-09-12 23:14:30 +0200

    enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,9 +8,10 @@ async function main() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe())
   app.useGlobalFilters(new HttpExceptionFilter())
-  //TODO: app.enableCors();
+  app.enableCors();
   await app.listen(process.env.BACKEND_PORT);
   console.log(`Application is running on: ${process.env.BACKEND_PORT}`)
 }
-main().then(() => console.log("Application started correctly."));
-  // .catch((err) => console.error(err));
+
+main().then(() => console.log("Application started correctly."))
+  .catch((err) => console.error(err));
