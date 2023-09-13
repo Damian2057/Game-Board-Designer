e9b92042edd5f3a2a610bdbb4f1568faf5d1418e
Damiaan <ddamian2057@gmail.com>
2023-09-13 22:27:33 +0200

    + information api module communication

--- backend/src/information/controller/information.controller.ts
+++ backend/src/information/controller/information.controller.ts
@@ -6,8 +6,6 @@ import { RolesGuard } from "../../auth/guard/roles.guard";
 import { InformationService } from "../service/information.service";
 import { InformationDto } from "../model/dto/information.dto";
 
-@HasRoles(UserRole.ADMIN)
-@UseGuards(JwtGuard, RolesGuard)
 @Controller('configuration')
 export class InformationController {
 
@@ -15,6 +13,8 @@ export class InformationController {
     private readonly informationService: InformationService
   ) {}
 
+  @HasRoles(UserRole.ADMIN)
+  @UseGuards(JwtGuard, RolesGuard)
   @Put('update/about')
   async updatePhone(@Query('address') address?: string,
                     @Query('phoneNumber') phoneNumber?: string,
